c1fc8049761d7585129c24ef5fe7cf89dd3cabd2
HerickReis <129025673+HerickReis@users.noreply.github.com>
2023-09-23 01:19:07 -0300

    Add files via upload

---
+++ Tuplas/pratica_tupla.py
@@ -0,0 +1,8 @@
+
+a = (2, 5, 3)
+b = (5, 8, 2, 1)
+c = b + a
+
+# sorted irá ordenar a lista de forma alfabética.
+# quando se utiliza o sinal de adição para tuplas elas nao irão somar os valores e sim irá juntar os valores
+# saída (2, 5, 3, 5, 8, 2, 1)
